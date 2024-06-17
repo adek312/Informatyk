1d9798b25db7dd86be7a3019ea8012f737ddcdd2
adek312 <adrian.dolata@vp.pl>
2024-06-17 19:24:57 +0200

    Add Top navigation bar

---
+++ informatyk/src/TopNavBar.js
@@ -0,0 +1,13 @@
+export default function TopNavBar(){
+  return (
+    <nav>
+      <div class="TopNavBar">
+        <ul>
+          <li>Start</li>
+          <li>INF02</li>
+          <li>INF03</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
