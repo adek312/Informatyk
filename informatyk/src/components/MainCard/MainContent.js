fad352d9db203013b5fa77f17db25cb02d94acee
adek312 <adrian.dolata@vp.pl>
2024-06-23 17:23:51 +0200

    Add main content section

--- informatyk/src/components/MainCard/MainContent.js
+++ informatyk/src/components/MainCard/MainContent.js
@@ -8,14 +8,18 @@ function MainContent() {
           <h2>Egzamin Teoretyczny</h2>
           <div />
           <div className={styles.MainContentDescription}>
-            <p>
-              Wybierz kwalifikacje i zacznij naukę do teorii.<br></br>
-              Więcej informacji o egzaminie teoretycznym:{" "}
-              <a href="strona">tutaj</a>
-              {/* Dodac do linku od razu scroll do sekcji egzamin teoria */}
-            </p>
-            <img src="https://via.placeholder.com/150" alt="INF02"></img>
-            <img src="https://via.placeholder.com/150" alt="INF03"></img>
+            <div className={styles.MainContentDescriptionText}>
+              <p>
+                Wybierz kwalifikacje i zacznij naukę do teorii.<br></br>
+                Więcej informacji o egzaminie teoretycznym znajdziesz:{" "}
+                <a href="strona"><span style={{color: "#58A4B0"}}>tutaj</span></a>
+                {/* Dodac do linku od razu scroll do sekcji egzamin teoria */}
+              </p>
+            </div>
+            <div className={styles.MainContentDescriptionImage}>
+              <img src="https://via.placeholder.com/350" alt="INF02"></img>
+              <img src="https://via.placeholder.com/350" alt="INF03"></img>
+            </div>
           </div>
         </div>
       </div>
